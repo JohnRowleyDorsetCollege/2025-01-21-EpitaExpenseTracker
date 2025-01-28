cc17413ad927ba2bc746452fdeb606ccbe2951b2
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-01-28 11:37:50 +0000

    added countries

--- Controllers/CascadingDemoController.cs
+++ Controllers/CascadingDemoController.cs
@@ -4,8 +4,20 @@ namespace EpitaExpenseTracker.Controllers
 {
     public class CascadingDemoController : Controller
     {
+
+
+        private static readonly Dictionary<string, List<string>> CountryStateData = new()
+    {
+        { "USA", new List<string> { "California", "Texas", "Florida" } },
+        { "Canada", new List<string> { "Ontario", "Quebec", "British Columbia" } },
+        { "India", new List<string> { "Maharashtra", "Karnataka", "Delhi" } }
+    };
+
+
+
         public IActionResult Index()
         {
+            ViewBag.Countries = CountryStateData.Keys;
             return View();
         }
     }
